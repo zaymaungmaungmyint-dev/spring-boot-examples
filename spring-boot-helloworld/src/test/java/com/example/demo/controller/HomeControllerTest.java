c6140f57d04ef0b8c0064c16ada40d07255e4beb
zaymaungmaungmyint <zaymaungmaungmyint@advent-soft.com>
2020-04-04 20:16:07 +0630

    add spring boot helloworld app

---
+++ spring-boot-helloworld/src/test/java/com/example/demo/controller/HomeControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void getHello() throws Exception {
+		/* mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greeting from Spring Boot !"))); */
+	}
+
+}
